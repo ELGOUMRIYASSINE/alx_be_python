6975262d37f438e819a159845d79b5e37f8f0219
yassine <proowork15@gmail.com>
2025-06-23 10:14:28 -0300

    new

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,40 +1,42 @@
+# library_system.py
+
 class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
 
-    def get_description(self):
+    def get_details(self):
         return f"Book: {self.title} by {self.author}"
 
 
-# Derived class - EBook
 class EBook(Book):
     def __init__(self, title, author, file_size):
-        super().__init__(title, author)  # Call the base constructor
-        self.file_size = file_size  # Unique to EBook
+        super().__init__(title, author)
+        self.file_size = file_size
 
-    def get_description(self):
+    def get_details(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 
-# Derived class - PrintBook
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
-        super().__init__(title, author)  # Inherit title, author
-        self.page_count = page_count  # Unique to PrintBook
+        super().__init__(title, author)
+        self.page_count = page_count
 
-    def get_description(self):
+    def get_details(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
 
 
-# Library class - uses composition
 class Library:
     def __init__(self):
-        self.books = []  # A list of Book/EBook/PrintBook objects
+        self.books = []
 
     def add_book(self, book):
-        self.books.append(book)  # Add any book type
+        if isinstance(book, Book):
+            self.books.append(book)
+        else:
+            print("Only instances of Book or its subclasses can be added.")
 
     def list_books(self):
         for book in self.books:
-            print(book.get_description())  # Calls the correct get_description for each
+            print(book.get_details())
